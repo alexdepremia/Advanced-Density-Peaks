668b0e446f09646cc82feb3e594373e11928f8be
arodrigu <arodrigu@hp6-cm-21.ictp.it>
2018-09-12 10:45:05 +0200

    Added unfinished scan Z feature

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 #mkdir bin
 cd ./src
 gfortran -ffree-line-length-0 -O3 ./rnkpar.f90 ./DPA.f90 -o ../bin/DPA.x
+gfortran -ffree-line-length-0 -O3 ./rnkpar.f90 ./DPA_scan.f90 -o ../bin/DPA_scan.x
 cd ..
